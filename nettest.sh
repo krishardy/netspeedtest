26fd902fba7fef4bb8b454e81d8d890debb1df1d
Kris Hardy <kris@rkrishardy.com>
2015-09-16 17:02:27 -0600

    Adding github downloads

--- nettest.sh
+++ nettest.sh
@@ -33,4 +33,44 @@ echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/1
 time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/1K.bin
 echo "---------------------------------------------------------------------"
 echo
+echo "/// 5K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/5K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/5K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 10K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/10K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/10K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 25K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/25K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/25K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 50K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/50K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/50K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 100K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/100K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/100K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 250K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/250K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/250K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 500K Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/500K.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/500K.bin
+echo "---------------------------------------------------------------------"
+echo
+echo "/// 1M Response ///"
+echo "$ time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/1M.bin"
+time curl -o /dev/null https://github.com/kris/netspeedtest/raw/master/1M.bin
+echo "---------------------------------------------------------------------"
+echo
 echo "Test complete."
